b898d583402191af325fe6ef7c5dc139e153bdb4
Ajay Chamarthy <axc41840@ucmo.edu>
2020-02-26 15:21:36 -0500

    Gulp - Autoprefixer, Css Minify and generate sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require("gulp");
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    autoPrefixer = require("gulp-autoprefixer"),
+    cssMin = require("gulp-cssmin"),
+    sourceMaps = require("gulp-sourcemaps");
 
-gulp.task("default", function() {
+gulp.task("css", function(done) {
+    //Compile sass
+    //Output file to a dist folder
+    gulp.src(["./src/scss/main.scss"])
+    .pipe(sourceMaps.init())
+    .pipe(sass().on("error",sass.logError))
+    .pipe(cssMin())
+    .pipe(autoPrefixer())
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest("./dist/css"));
+    done();
+});
+
+gulp.task("default", async function() {
     console.log("Look at the gulp console");
 });
